efe4fcecebdc8ee8ec50a875619d530c4901a9dc
BlakeGeist <rightfoot@gmail.com>
2017-12-16 02:59:55 -0800

    update admin page
    update admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,7 @@ class AdminController < ApplicationController
   before_action :auth_user, only: [:index]
   def index
     @sites = Site.order('created_at DESC')
+    @site = Site.find_by(name: request.domain)
     @new_site = Site.new
     @products = Product.joins(:product_details).where(product_details: {name: 'active'}).where(product_details: {value: '1'}).order('created_at').paginate(:page => params[:page], :per_page => 30)
   end
