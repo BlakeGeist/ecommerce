1f2146b4ed696c5a55ffdf30dacb60250db928b4
BlakeGeist <rightfoot@gmail.com>
2017-12-20 04:06:40 -0800

    update category image
    update category image

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,7 +1,7 @@
 module CategoriesHelper
 
   def category_product(category)
-    first_cat_product = Product.joins(:product_categories).where(product_categories: {category_id: category.id}).first
+    first_cat_product = Product.where(id: @site.site_products.map(&:product_id)).where(product_categories: {category_id: category.id}).joins(:product_categories).uniq.first
     return first_cat_product
   end
 
