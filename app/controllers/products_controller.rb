7ca5a849c25f39e4849d6cc78546f1590addf83d
BlakeGeist <rightfoot@gmail.com>
2017-12-21 00:20:17 -0800

    update prodcut show page
    update prodcut show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,12 +24,15 @@ class ProductsController < ApplicationController
      @categories = Category.where(id: @site.site_categories.map(&:category_id))
      @brands = Brand.where(id: @site.site_brands.map(&:brand_id))
      @product = Product.friendly.find(params[:id])
-     @product.product_categories.first.category_id
-     @product_description_length = @product.product_details.where(product_details: {name: 'description'})[0]['value'].length
-     if @product_description_length < 550
-       amount = 2
-     else
-       amount = 4
+     amount = 2
+     if @product.product_categories.first
+       @product.product_categories.first.category_id
+       @product_description_length = @product.product_details.where(product_details: {name: 'description'})[0]['value'].length
+       if @product_description_length < 550
+         amount = 2
+       else
+         amount = 4
+       end
      end
      @products = Product.where(id: @site.site_products.map(&:product_id)).where(product_categories: {category_id: @product.product_categories.first.category_id}).joins(:product_categories).uniq.first(amount)
      @title = "#{@product.name}"
